67912d6229e7dda51334ab42b88a01f7b9700ee6
Timo Korvola <tkorvola@iki.fi>
2015-09-09 22:16:31 +0300

    More clang setup.
    I hope that the HeaderSearch stuff will not be necessary.

--- snavigator/parsers/clang/parser.cc
+++ snavigator/parsers/clang/parser.cc
@@ -1,6 +1,11 @@
+#include <memory>
+
+#include <llvm/Support/Host.h>
 #include <llvm/ADT/IntrusiveRefCntPtr.h>
+#include <clang/Basic/TargetInfo.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/TextDiagnosticBuffer.h>
+// #include <clang/Lex/HeaderSearch.h>
 
 extern "C" {
 #include "snptools.h"
@@ -17,11 +22,23 @@ class cppbrowser::Parser_impl
 public:
   CompilerInstance ci;
   TextDiagnosticBuffer buf;
+  std::shared_ptr<TargetOptions> to;
+  llvm::IntrusiveRefCntPtr<HeaderSearchOptions> hso;
+  //std::unique_ptr<HeaderSearch> hs;
 
-  Parser_impl() {
-    ci.createDiagnostics(&buf);
+  Parser_impl():
+    to(std::make_shared<TargetOptions>()),
+    hso(new HeaderSearchOptions)
+  {
+    ci.createDiagnostics(&buf, false);
     ci.createFileManager();
     ci.createSourceManager(ci.getFileManager());
+    to->Triple = llvm::sys::getDefaultTargetTriple();
+    ci.setTarget(TargetInfo::CreateTargetInfo(ci.getDiagnostics(), to));
+    // hs.reset(
+    //   new HeaderSearch(
+    // 	hso, ci.getSourceManager(), ci.getDiagnostics(), ci.getLangOpts(),
+    // 	TargetInfo::CreateTargetInfo(ci.getDiagnostics(), ci.getTargetOpts())));
   }
 };
 
@@ -35,7 +52,9 @@ cppbrowser::Parser::~Parser()
 int
 cppbrowser::Parser::parse(const char *filename)
 {
-  //TODO
+  // Or maybe TU_Prefix for headers?
+  impl->ci.createPreprocessor(clang::TU_Complete);
+  impl->ci.getPreprocessorOpts().UsePredefines = true;
 }
 
 void
