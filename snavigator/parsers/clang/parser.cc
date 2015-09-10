5d94f985e63963ddca5b402b22e7b44e86b19593
Timo Korvola <tkorvola@iki.fi>
2015-09-11 00:52:25 +0300

    AST visitor and stuff.

--- snavigator/parsers/clang/parser.cc
+++ snavigator/parsers/clang/parser.cc
@@ -1,10 +1,16 @@
 #include <memory>
+#include <iostream>
+#include <string>
 
 #include <llvm/Support/Host.h>
 #include <clang/Basic/TargetInfo.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/TextDiagnosticBuffer.h>
 #include <clang/Lex/HeaderSearch.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/ASTContext.h>
+#include <clang/Parse/ParseAST.h>
 
 extern "C" {
 #include "snptools.h"
@@ -12,6 +18,7 @@ extern "C" {
 
 #include "parser.hh"
 
+using namespace std;
 using namespace clang;
 
 using namespace cppbrowser;
@@ -25,8 +32,7 @@ public:
   Parser_impl() {
     ci.createDiagnostics(&buf, false);
     ci.createFileManager();
-    ci.createSourceManager(ci.getFileManager());
-    std::shared_ptr<TargetOptions> to = std::make_shared<TargetOptions>();
+    shared_ptr<TargetOptions> to = make_shared<TargetOptions>();
     to->Triple = llvm::sys::getDefaultTargetTriple();
     ci.setTarget(TargetInfo::CreateTargetInfo(ci.getDiagnostics(), to));
 
@@ -44,13 +50,60 @@ cppbrowser::Parser::Parser():
 cppbrowser::Parser::~Parser()
 {}
 
+namespace {
+  class SN_AST_visitor:
+  public RecursiveASTVisitor<SN_AST_visitor>
+  {
+  public:
+    //TODO
+    bool VisitFunctionDecl(FunctionDecl *f) {
+      int type = f->isThisDeclarationADefinition() ? SN_FUNC_DEF : SN_FUNC_DCL;
+      string id = f->getNameInfo().getAsString();
+      cout << type << id << endl;
+    }
+  };
+
+  class SN_AST_consumer:
+  public ASTConsumer
+  {
+  public:
+
+#if 1
+    // Process as we read.
+    bool HandleTopLevelDecl(DeclGroupRef group) override {
+      for (DeclGroupRef::iterator d = group.begin();
+	   d != group.end(); ++d)
+	vis.TraverseDecl(*d);
+      return true;
+    }
+#else
+    // Process after the whole translation unit has been read.
+    void HandleTranslationUnit(ASTContext &ctx) override {
+      vis.TraverseDecl(ctx.getTranslationUnitDecl());
+    }
+#endif
+
+  private:
+    SN_AST_visitor vis;
+  };
+}
+
 int
 cppbrowser::Parser::parse(const char *filename)
 {
   CompilerInstance &ci = impl->ci;
+  ci.createSourceManager(ci.getFileManager());
+  SourceManager &sm = ci.getSourceManager();
+  sm.setMainFileID(sm.createFileID(ci.getFileManager().getFile(filename),
+				   SourceLocation(), SrcMgr::C_User));
   // Or maybe TU_Prefix for headers?
   ci.createPreprocessor(clang::TU_Complete);
+  Preprocessor &pp = ci.getPreprocessor();
   ci.getPreprocessorOpts().UsePredefines = true;
+  ci.setASTConsumer(new SN_AST_consumer);
+  ci.createASTContext();
+  ci.getDiagnosticClient().BeginSourceFile(ci.getLangOpts(), &pp);
+  ParseAST(pp, &ci.getASTConsumer(), ci.getASTContext());
 }
 
 void
